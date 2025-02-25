62b4af36c3ab43988ca324045a4bdad94dbe99af
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-25 16:14:16 -0300

    codigo arrumado

--- buscante-alura/src/app/views/lista-livros/lista-livros.component.ts
+++ buscante-alura/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/Interfaces';
+import { Item, Livro } from 'src/app/models/Interfaces';
+import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -24,24 +25,10 @@ export class ListaLivrosComponent implements OnDestroy {
     });
   }
 
-  livroResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = [];
-
-    items.forEach((item) => {
-      livros.push(
-        (this.livro = {
-          title: item.volumeInfo?.title,
-          authors: item.volumeInfo?.authors,
-          publisher: item.volumeInfo?.publisher,
-          publishedDate: item.volumeInfo?.publishedDate,
-          description: item.volumeInfo?.description,
-          previewLink: item.volumeInfo?.previewLink,
-          thumbnail: item.volumeInfo?.imageLinks?.thumbnail,
-        })
-      );
-    });
-
-    return livros;
+  livroResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
   }
 
   ngOnDestroy(): void {
