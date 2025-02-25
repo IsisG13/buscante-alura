3b5a8b9aaab14997638b7f3801bd8b0ac835e235
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-25 15:39:57 -0300

    cards corretos na tela

--- buscante-alura/src/app/views/lista-livros/lista-livros.component.ts
+++ buscante-alura/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/Interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -8,20 +9,42 @@ import { LivroService } from 'src/app/service/livro.service';
   styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
   subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) {}
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (retornoApi) => console.log(),
-      error: erro => console.error(erro)
+      next: (items) =>
+        (this.listaLivros = this.livroResultadoParaLivros(items)),
+      error: (erro) => console.error(erro),
     });
+  }
+
+  livroResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = [];
+
+    items.forEach((item) => {
+      livros.push(
+        (this.livro = {
+          title: item.volumeInfo?.title,
+          authors: item.volumeInfo?.authors,
+          publisher: item.volumeInfo?.publisher,
+          publishedDate: item.volumeInfo?.publishedDate,
+          description: item.volumeInfo?.description,
+          previewLink: item.volumeInfo?.previewLink,
+          thumbnail: item.volumeInfo?.imageLinks?.thumbnail,
+        })
+      );
+    });
+
+    return livros;
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
-    }
+  }
 }
